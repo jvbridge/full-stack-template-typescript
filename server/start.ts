1cf849c40c9fa9d5b112d60a8989c27a03e68c17
Jonathan Bridge <jvbridge@gmail.com>
2022-07-12 12:02:51 -0700

    import port const

--- server/start.ts
+++ server/start.ts
@@ -1,8 +1,8 @@
 import { app, server } from './server';
 import { DocumentNode } from 'graphql';
+import { PORT } from './config/consts';
 
 // the port we will be on will be 3001 or whatever the service we use has
-const PORT = process.env.PORT || 3001;
 
 const start = async (typedefs: DocumentNode, resolvers: any) => {
   await server.start();
