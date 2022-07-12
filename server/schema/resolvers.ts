949eaff30be9060512d5e8b084c0d10d78466b6f
Jonathan Bridge <jvbridge@gmail.com>
2022-07-12 12:52:04 -0700

    create user

--- server/schema/resolvers.ts
+++ server/schema/resolvers.ts
@@ -6,11 +6,19 @@ const resolvers = {
   Query: {
     me: async (parent: any, args: any, context: any) => {
       if (context.user) {
-        return User.findById(context.user._id)
-          .populate('checklists')
-          .populate('todo');
+        return User.findById(context.user._id);
       }
       throw new AuthenticationError('You need to be logged in!');
+    },
+    user: async (parent: any, args: { email: string }) => {
+      const user = await User.find({ email: args.email });
+      if (user) return user;
+      throw new UserInputError('could not find user with that email');
+    },
+  },
+  Mutation: {
+    adduser: async (parent: any, args: { email: string; password: string }) => {
+      return User.create({ args });
     },
   },
 };
