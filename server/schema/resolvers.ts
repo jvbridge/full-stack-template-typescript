6130629d73dd1406ec9af266b14b8ab4d246c3a5
Jonathan Bridge <jvbridge@gmail.com>
2022-07-12 12:27:58 -0700

    fixed imports

--- server/schema/resolvers.ts
+++ server/schema/resolvers.ts
@@ -1,3 +1,7 @@
 import { AuthenticationError, UserInputError } from 'apollo-server-express';
-import User from '../models/User';
-import { signToken } from '../util/auth';
+import User from '../models';
+import auth from '../util';
+
+const resolvers = {};
+
+export default resolvers;
