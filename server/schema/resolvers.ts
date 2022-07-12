c037d2570285e61cd3a3a440078994dd26b5bf81
Jonathan Bridge <jvbridge@gmail.com>
2022-07-12 12:43:15 -0700

    me query

--- server/schema/resolvers.ts
+++ server/schema/resolvers.ts
@@ -2,6 +2,17 @@ import { AuthenticationError, UserInputError } from 'apollo-server-express';
 import User from '../models';
 import auth from '../util';
 
-const resolvers = {};
+const resolvers = {
+  Query: {
+    me: async (parent: any, args: any, context: any) => {
+      if (context.user) {
+        return User.findById(context.user._id)
+          .populate('checklists')
+          .populate('todo');
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+};
 
 export default resolvers;
