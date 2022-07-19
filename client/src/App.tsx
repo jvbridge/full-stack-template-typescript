271b6ee115c021587c227fa30eb07faaab7f2ad3
Jonathan Bridge <jvbridge@gmail.com>
2022-07-19 14:34:02 -0700

    react router install

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
