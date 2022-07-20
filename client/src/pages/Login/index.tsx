ca741452b4d1e9179e68db48b217f86b08b750e7
Jonathan Bridge <jvbridge@gmail.com>
2022-07-19 18:23:04 -0700

    error boundary on card

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,3 +1,26 @@
+// import { FormEvent, useState } from 'react';
+import { Card } from 'react-bootstrap';
+import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
+// import auth from '../../util/auth';
+
+function ErrorFallBack({ error, resetErrorBoundary }: FallbackProps) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+}
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <ErrorBoundary FallbackComponent={ErrorFallBack}>
+      <div>login</div>
+      <Card>
+        {/* <Card.Header>Login or sign up!</Card.Header> */}
+        <Card.Body>Stuff goes here</Card.Body>
+      </Card>
+    </ErrorBoundary>
+  );
 }
